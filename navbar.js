cd3d6cf12389c2139f1cc9322321105c483b10be
JayQkew <rayjebb@gmail.com>
2024-09-21 18:46:19 +0200

    adding comments to navbar code

--- navbar.js
+++ navbar.js
@@ -10,14 +10,20 @@ const pages = [
 
 console.log(currPage);
 
+/**
+ * creates a 'ul' of links to other pages of the website
+ * the url's are declared in the 'pages' array using objects
+ * @param {String} currPage the current page the user is on
+ */
 function createNav(currPage){
     const list = document.createElement('ul');
     let allPages = '';
 
     pages.map( p => {
         let listItem;
-        let url = `${(currPage == 'Home') ? '':'/'}${p.url}`;
+        let url = `${(currPage == 'Home') ? '':'/'}${p.url}`;   // manages the folder hierarchy for the url
 
+        // ensures the the current page doesnt have a hyperlink
         if(p.page == currPage){
             listItem = `
                 <li>
@@ -33,11 +39,25 @@ function createNav(currPage){
             `;
         }
 
+        // appends it to the string of all the pages
         allPages += listItem;
     })
-
+    // adds the string as HTML to the 'ul'
     list.innerHTML = allPages;
     nav.appendChild(list);
+}
+
+/**
+ * creates the decorative elements of the navbar
+ * @param {String} side left or right side of the navbar
+ */
+function addDecoration(side){
+    const decoration = document.createElement('div');
+    const starSVG = document.createElement('svg');
+    const line = document.createElement('div');
+    
+    decoration.appendChild(side === 'left' ? starSVG : line);
+    decoration.appendChild(side === 'left' ? line : starSVG);
 }
 
 createNav(currPage);
