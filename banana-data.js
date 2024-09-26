adac5f531fafb82fd5ac5a457581268353a0dee9
JayQkew <rayjebb@gmail.com>
2024-09-26 11:37:29 +0200

    banana script

---
+++ banana-data.js
@@ -0,0 +1,32 @@
+const bananaData = {
+    mass: 120,       // Average mass in grams
+    length: 18,      // Average length in centimeters
+    radius: 2.5,     // Average radius in centimeters
+    volume: 118,     // Approximate volume in cubic centimeters
+    density: 0.96,   // Approximate density in g/cm³
+    discovered: 1836 // Year of discovery/first recorded
+};
+
+/**
+ * converts data to a higher metric system
+ * @param {Number} data data to convert
+ * @param {String} currUnit unit of that data
+ */
+function unitConversion(data, currUnit){
+    if(currUnit === 'g'){
+        //to kg
+        return data/1000;
+    }
+    else if(currUnit === 'cm'){
+        //to km
+        return data/100000;
+    }
+    else if(currUnit === 'cm3'){
+        //to km^3
+        return data/1000000000000000;
+    }
+
+    return console.log(`${currUnit} doesnt need conversion`)
+}
+
+export default bananaStuff;
