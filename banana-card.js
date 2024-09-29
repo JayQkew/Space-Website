435a174f39e93bfb970e6a10b038071f26aa1822
JayQkew <rayjebb@gmail.com>
2024-09-29 14:36:27 +0200

    home page styling complete

--- banana-card.js
+++ banana-card.js
@@ -9,10 +9,32 @@ function fillData(){
     bananaStats.map(stats =>{
         //stats[0] is the key
         //stats[1] is the value
+
+        let unitOfMeasurement;
+
+        switch(stats[0]){
+            case 'mass':
+                unitOfMeasurement = 'g';
+                break;
+            case 'length':
+            case 'radius':
+                unitOfMeasurement = 'cm';
+                break;
+            case 'volume':
+                unitOfMeasurement = 'cm³';
+                break;
+            case 'density':
+                unitOfMeasurement = 'g/cm³';
+                break;
+            default:
+                unitOfMeasurement = '';
+                break;
+        }
+
         let stat = `
             <li>
                 <p>${stats[0]}</p>
-                <p>${stats[1]}</p>
+                <p>${stats[1]} ${unitOfMeasurement}</p>
             </li>
         `
         ul.innerHTML += stat;
