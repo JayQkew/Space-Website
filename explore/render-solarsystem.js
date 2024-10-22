88309e6a341553af33b98cbe6a88bee268dcc82c
JayQkew <rayjebb@gmail.com>
2024-10-22 07:13:10 +0200

    scroll animations refractor

--- explore/render-solarsystem.js
+++ explore/render-solarsystem.js
@@ -5,6 +5,7 @@ const planetData = await getPlanetData();
 
 const SUNRADIUS = 100;
 const ZOOM = d3.zoom().on('zoom', e => svg.attr('transform', e.transform));
+const SCROLLTHRESHHOLD = 1; // scrollValue threshold before zoom transition
 
 let extentRadius = getRadiusExtent(),
     extentDistance = getDistanceExtent();
@@ -13,8 +14,11 @@ let RADIUS = window.innerWidth,
     MARGIN = 50;
 
 let svg;
-let planets;
-let planetTexts;
+let planets, planetTexts;
+let minPlanetAngle = 4,
+    maxPlanetAngle = 3.75,
+    planetAngleBarrier = 5;
+let scrollValue;
 
 /**
  * gets the extents of the distance from the sun
@@ -67,12 +71,6 @@ function renderSolarSystem(){
     .enter()
     .append('circle')
     .attr('class', d => d.englishName.toLowerCase() + ' planet')
-    .attr('cx', d => {
-        return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.cos(aScale(d.semimajorAxis)*Math.PI/2);
-    })
-    .attr('cy', d => {
-        return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.sin(aScale(d.semimajorAxis)*Math.PI/2);
-    })
     .attr('r', d => {
         return (d.englishName === 'Sun') ? SUNRADIUS : rScale(d.meanRadius);
     });
@@ -84,15 +82,9 @@ function renderSolarSystem(){
     .enter()
     .append('text')
     .attr('class', d => `${d.englishName.toLowerCase()}-label planet-label`)
-    .attr('x', d => {
-        return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.cos(aScale(d.semimajorAxis) * Math.PI / 2);
-    })
-    .attr('y', d => {
-        let yPos = (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.sin(aScale(d.semimajorAxis) * Math.PI / 2);
-        let r = (d.englishName == 'Sun') ? SUNRADIUS : rScale(d.meanRadius);
-        return yPos - r - 15;
-    })
     .text(d => d.englishName);
+
+    updatePositions();
 }
 
 /**
@@ -137,6 +129,82 @@ const rScale = d3.scaleLinear()
 // angle scale
 const aScale = d3.scaleLinear()
 .domain([extentDistance[0], extentDistance[1]])
-.range([4, 3.75]);
+.range([minPlanetAngle, maxPlanetAngle]);
+
+/**
+ * changes the planets angles based on scroll
+ * the planets will move along their respective rings and eventually align bellow the sun
+ */
+function updatePlanetAngle(){
+    scrollValue = window.scrollY / RADIUS*2;
+
+    let angleRangeStart = minPlanetAngle + ((planetAngleBarrier-minPlanetAngle) * scrollValue);
+    let angleRangeEnd = maxPlanetAngle + ((planetAngleBarrier-maxPlanetAngle) * scrollValue);
+
+    aScale.range([angleRangeStart, angleRangeEnd]);
+}
+
+/**
+ * sets the zoom positions of the svg and zoom to their starting positions
+ */
+function solarSystemZoomStart(){
+    d3.select('.ss-inner')
+    .transition()
+    .duration(1200)
+    .attr('transform', `translate(${MARGIN/2},${RADIUS})`)
+    .call(ZOOM.transform, d3.zoomIdentity.translate(0 ,0))
+    .on('start', () => {
+        d3.zoomIdentity.x = MARGIN/2;
+        d3.zoomIdentity.y = RADIUS;
+    });
+
+    d3.select('.solar-system')
+    .transition()
+    .duration(1200)
+    .attr('height', RADIUS*2.5);
+}
+
+/**
+ * updates the positions of the planets and planetTexts
+ */
+function updatePositions(){
+    if(scrollValue >= SCROLLTHRESHHOLD){
+        planets.attr('cx', d => {
+            return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.cos(5 * Math.PI / 2);
+        })
+        .attr('cy', d => {
+            return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.sin(5 * Math.PI / 2);
+        });
+
+        //error prevention
+        if(typeof planetText !== 'undefined') planetText?.style('display', 'none');
+    }
+    else{
+        planets.attr('cx', d => {
+            return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.cos(aScale(d.semimajorAxis) * Math.PI / 2);
+        })
+        .attr('cy', d => {
+            return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.sin(aScale(d.semimajorAxis) * Math.PI / 2);
+        });
+    
+        planetTexts.attr('x', d => {
+            return (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.cos(aScale(d.semimajorAxis) * Math.PI / 2);
+        })
+        .attr('y', d => {
+            let yPos = (d.englishName === 'Sun') ? 0 : dScale(d.semimajorAxis) * Math.sin(aScale(d.semimajorAxis) * Math.PI / 2);
+            let radius = (d.englishName === 'Sun') ? SUNRADIUS : rScale(d.meanRadius);
+            return yPos - radius - 15;
+        });
+    }
+}
 
 renderSolarSystem();
+
+window.addEventListener('scroll', () => {
+    updatePlanetAngle();
+    updatePositions();
+    if(scrollValue <= SCROLLTHRESHHOLD){
+        // planetStatsCard.style.display = 'none';
+        solarSystemZoomStart();
+    }
+})
