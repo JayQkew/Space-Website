56f6fa4e76eddf37f3b4beff95ff7f026d5bbf67
JayQkew <rayjebb@gmail.com>
2024-10-23 23:02:49 +0200

    bubble forces

--- explore/planet-basket.js
+++ explore/planet-basket.js
@@ -19,39 +19,24 @@ function createSVG(){
     }
 }
 
-//#region
-function createBubble(){
-    bubbles = svg.selectAll('circle')
-    .data(planetBasket, d => d.englishName)
-    .join(
-        enter => enter.append('circle')
-            .attr('r', d => rScale(d.meanRadius))
-            .attr('class', d => d.englishName.toLowerCase() + ' planet'),
-        update => update,
-        exit => exit.remove()
-    );
-
-    labels = svg.selectAll('text.planet-label')
-    .data(planetBasket, d => d.englishName)
-    .join(
-        enter => enter.append('text')
-            .attr('class', 'planet-label')
-            .attr('text-anchor', 'middle')
-            .text(d => d.englishName),
-        update => update,
-        exit => exit.remove()
-    );
-
-    simulation.nodes(planetBasket).on('tick', () => {
-        bubbles.attr('cx', d => d.x)
-        .attr('cy', d => d.y);
-
-        labels.attr('x', d => d.x)
-        .attr('y', d => d.y + rScale(d.meanRadius) + 15);
+function createSortBtns(){
+    const sortBtns = document.querySelector('.sort-btn-container');
+    sortBtns.innerHTML = '';
+    Xforces.forEach(force =>{
+        let btn = document.createElement('button');
+        btn.classList.add('sort-btn');
+        btn.setAttribute('data-type', force.name);
+        btn.innerHTML = force.name;
+        btn.addEventListener('click', () =>{
+            simulation.force('x', force.force)
+            .alpha(1)
+            .restart();
+        })
+        sortBtns.appendChild(btn);
     })
-
-    simulation.alpha(1).restart();
 }
+
+//#region
 
 /**
  * adds the force or updates the force if it exists already
@@ -62,6 +47,7 @@ function addForce(name, force){
     const existingForce = Xforces.find(f => f.name === name);
     if (!existingForce) {
         Xforces.push({ name, force });
+        console.log(force);
     }
 }
 
@@ -101,88 +87,25 @@ const rScale = d3.scaleLinear()
 .domain(d3.extent(planetData, d => d.meanRadius))
 .range([18, 210]);
 
-const collideForce = d3.forceCollide(d => rScale(d.meanRadius));
-const manyBody = d3.forceManyBody().strength(-100);
+let simulation = d3.forceSimulation()
+.force('x', d3.forceX(window.innerWidth / 2).strength(0.04))
+.force('y', d3.forceY(window.innerHeight / 3).strength(0.01))
+.force('collide', d3.forceCollide(d => rScale(d.meanRadius)))
+.force('manyBody', d3.forceManyBody().strength(-100));
 
-const forceY = d3.forceY(window.innerHeight / 3).strength(0.01);
-
-const simulation = d3.forceSimulation()
-.force('x', d3.forceX(window.innerWidth / 2).strength(0.04))  // Safely access the first force
-.force('y', forceY)
-.force('collider', collideForce)
-.force('manyBody', manyBody);
-
-
-simulation.alpha(1).restart();
-createSVG();
-updateForces();
+// createSVG();
+// updateForces();
 //#endregion
 
 export function createBubbles() {
     createSVG();
+    updateForces();
+    createSortBtns();
 
-    // #region Scales
-    const rScale = d3.scaleLinear()
-    .domain(d3.extent(planetData, d => d.meanRadius))
-    .range([18, 210]);
 
-    const mScale = d3.scaleLinear()
-    .domain(d3.extent(planetBasket, d => massNum(d)))
-    .range([window.innerWidth / 4, window.innerWidth - window.innerWidth / 4]);
-    
-    const _rScale = d3.scaleLinear()
-    .domain(d3.extent(planetBasket, d => d.meanRadius))
-    .range([window.innerWidth / 4, window.innerWidth - window.innerWidth / 4]);
-    
-    const vScale = d3.scaleLinear()
-    .domain(d3.extent(planetBasket, d => volumeNum(d)))
-    .range([window.innerWidth / 4, window.innerWidth - window.innerWidth / 4]);
-    
-    const dScale = d3.scaleLinear()
-    .domain(d3.extent(planetBasket, d => d.density))
-    .range([window.innerWidth / 4, window.innerWidth - window.innerWidth / 4]);
-    
-    const gScale = d3.scaleLinear()
-    .domain(d3.extent(planetBasket, d => d.gravity))
-    .range([window.innerWidth / 4, window.innerWidth - window.innerWidth / 4]);
-    // #endregion
-
-    // #region adding forces
-    const forceY = d3.forceY(window.innerHeight / 3).strength(0.01);
-    const collideForce = d3.forceCollide(d => rScale(d.meanRadius));
-    const manyBody = d3.forceManyBody().strength(-100);
-    
-    addForce('Regular', d3.forceX(window.innerWidth / 2).strength(0.04));
-    addForce('Mass', d3.forceX(d => mScale(massNum(d))).strength(0.15));
-    addForce('Radius', d3.forceX(d => _rScale(d.meanRadius)).strength(0.15));
-    addForce('Volume', d3.forceX(d => vScale(volumeNum(d))).strength(0.15));
-    addForce('Density', d3.forceX(d => dScale(d.density)).strength(0.15));
-    addForce('Gravity', d3.forceX(d => gScale(d.gravity)).strength(0.15));
-    // #endregion
-
-    const sortBtns = document.querySelector('.sort-btn-container');
-    sortBtns.innerHTML = '';
-    Xforces.map(force =>{
-        let btn = document.createElement('button');
-        btn.classList.add('sort-btn');
-        btn.setAttribute('data-type', force.name);
-        btn.innerHTML = force.name;
-        btn.addEventListener('click', () =>{
-            simulation.force('x', force.force)
-            .alpha(1)
-            .restart();
-        })
-        sortBtns.appendChild(btn);
-    })
-
-    const simulation = d3.forceSimulation()
-        .force('x', d3.forceX(window.innerWidth / 2).strength(0.04))
-        .force('y', forceY)
-        .force('collide', collideForce)
-        .force('manyBody', manyBody);
 
     // Bind data to circles, using 'join' to handle enter, update, and exit selections
-    let bubbles = svg
+    bubbles = svg
         .selectAll('circle')
         .data(planetBasket, d => d.englishName)  // Use a unique key (e.g., id or name)
         .join(
@@ -193,7 +116,7 @@ export function createBubbles() {
             exit => exit.remove()  // Remove unneeded circles
         );
 
-    let labels = svg
+    labels = svg
     .selectAll('text.planet-label')
     .data(planetBasket, d => d.englishName)
     .join(
