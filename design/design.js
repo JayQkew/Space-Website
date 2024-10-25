3852b324a9c78d9bf05625c3fd0b29fa354c6eda
JayQkew <rayjebb@gmail.com>
2024-10-25 10:38:21 +0200

    design wireframe object

---
+++ design/design.js
@@ -0,0 +1,41 @@
+const wireframeArticle = document.querySelector('.wireframes');
+
+const wireframes = [
+    {
+        pageName: "Home Page",
+        description: "This is a brief description of the design.",
+        midFid: "/images/Home Page-mid-fid.png",
+        highFid: "/images/Home Page-high-fid.png",
+        midAlt: "Mid-fidelity wireframe for the Home Page",
+        highAlt: "High-fidelity wireframe for the Home Page"
+    },
+    {
+        pageName: "Explore Page",
+        description: "This is a brief description of the design.",
+        midFid: "/images/Explore Page-mid-fid.png",
+        highFid: "/images/Explore Page-high-fid.png",
+        midAlt: "Mid-fidelity wireframe for the Explore Page",
+        highAlt: "High-fidelity wireframe for the Explore Page"
+    },
+    {
+        pageName: "Design Page",
+        description: "This is a brief description of the design.",
+        midFid: "/images/Design Page-mid-fid.png",
+        highFid: "/images/Design Page-high-fid.png",
+        midAlt: "Mid-fidelity wireframe for the Design Page",
+        highAlt: "High-fidelity wireframe for the Design Page"
+    },
+    {
+        pageName: "About Page",
+        description: "This is a brief description of the design.",
+        midFid: "/images/About Page-mid-fid.png",
+        highFid: "/images/About Page-high-fid.png",
+        midAlt: "Mid-fidelity wireframe for the About Page",
+        highAlt: "High-fidelity wireframe for the About Page"
+    }
+];
+
+
+function createWireframes(){
+
+}
